9292c240009dd514d1feb2708f039018797bf7e8
URLeeo <mammadzadaaslan05@gmail.com>
2026-03-06 14:48:45 +0400

    Added main structure

---
+++ IronSchool_HomeWork/src/test/java/org/example/ironschool_homework/IronSchoolHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ironschool_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronSchoolHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
